753bd95203f4efb6e5eeac5bc13bc4e365843c61
Jane Dalton (nb0959) <nb0959@LM236JHYQG.local>
2023-04-19 15:45:09 +0100

    react server component

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 import Image from "next/image";
-import { Inter } from "next/font/google";
 
 // adding async turns this into a React server component
 export default async function Home() {
   // get data
-  const res = await fetch("http://localhost:1337/api/sb-product-details-live");
+  const res = await fetch(
+    "https://strapi-v4.qa.webcms-dev.digital.nbrown.co.uk/api/sb-homepage"
+  );
   const data = await res.json();
-  console.log(data);
 
   return (
     <main>
@@ -19,11 +19,13 @@ export default async function Home() {
           height={37}
           priority
         />
-        <div>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-        </div>
+
+        <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
       </h1>
+      <br />
       <h2> Using latest experimental features</h2>
+      <br />
+      <h1>{data.seoContent.title}</h1>
     </main>
   );
 }
