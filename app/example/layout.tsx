367da1e56272b96c2b5d8ae7cbcac68850fe7e4d
Jane Dalton (nb0959) <nb0959@LM236JHYQG.local>
2023-04-21 16:51:05 +0100

    improved nested layout example

--- app/example/layout.tsx
+++ app/example/layout.tsx
@@ -7,10 +7,18 @@ export default function ExampleLayout({
     <div>
       <div className="nested_main">
         <h3>Example Nested Layout</h3>
+        <h4>
+          so the /example/page.tsx for this example is being rendered here in
+          this nested layout
+        </h4>
         {children}
       </div>
       <div className="sidebar">
         <h2>sidebar</h2>
+        <p>
+          we can have lots of things here, such as client side rendered
+          components. Next 13 can be very flexable with complex layouts
+        </p>
       </div>
     </div>
   );
--- app/example/page.tsx
+++ app/example/page.tsx
@@ -5,6 +5,12 @@ export default function example() {
     <div>
       <article>
         <h1>An Example Page</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa
+          excepturi et nostrum! Quae pariatur, maxime itaque voluptatem nisi
+          illo eveniet id velit debitis odit perferendis voluptatum sapiente,
+          ut, alias similique!
+        </p>
       </article>
     </div>
   );
